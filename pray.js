2bc2f48a6738109bed9ee1f5a4ed7450dafd829f
Chocola <55474977+ChocolaJS@users.noreply.github.com>
2019-09-21 00:12:48 +0900

    Update pray.js

--- pray.js
+++ pray.js
@@ -12,6 +12,26 @@ client.on("ready", () => {
 
 client.on("message", message => {
   if( !message.guild ) return;
+  if( message.content.startsWith(config.prefix) ){
+    const key = message.content.slice(config.prefix.length).split(" ");
+    switch(key[0]){
+      case "setchannel":
+        const channel = message.mentions.channels.first() || client.channels.find(c => c.type=="text" && (c.id==key[1]||c.name==key[1]));
+        if( channel ){
+          option.playing = channel.id;
+          message.channel.send(`戦闘チャンネルを「${channel}」に変更しました！`);
+        }else{
+          message.channel.send(`そのチャンネルはありません！`);
+        }
+        break;
+      case "stop":
+        option.playing = "";
+        message.channel.send(`停止します！`);
+        break;
+      default:
+        message.channel.send(`::${key.join(" ")}`);
+    }
+  }
   if( message.author.id=="526620171658330112" ){
     if( message.content.match("の攻撃！") && message.content.match("はやられてしまった。。。") ){
       let split = message.content.split(/の攻撃！|\s-\s|のHP\:|\n|はやられてしまった。。。/);
